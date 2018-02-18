ec414fe3dd9ad808375ff91194c3381425725d5e
fcabrera <facucabrera94@gmail.com>
2018-02-18 12:39:48 -0300

    Mas cambios esteticos

--- Software/AI/Red Neuronal Multicapa/Back Propagation/nn.m
+++ Software/AI/Red Neuronal Multicapa/Back Propagation/nn.m
@@ -1,24 +1,21 @@
 %% Create Neural Network
 
-In_Dim = 2;
-Hidden_Dim = 10;
-Out_Dim = 1;
+In_Dim = 1; %numero de filas, siempre debe ser una columna.
+Hidden_Dim = 1;
+Out_Dim = 1; %numero de filas, siempre debe ser una columna.
 
 [w1,w2] = Create(In_Dim,Hidden_Dim,Out_Dim);
 
 %%  Training
 
-Epocas = 50;
-Learning_Rate = 1;
+Epocas = 2;
+Learning_Rate = 10;
 %In_Set = [1;2,2;4,4;8];
-Target_Set = [3;6;12];
-
-[w1,w2,Error] = BackPropagation(In_Set,Target_Set,Learning_Rate,w1,w2,Epocas);
-disp(w1,w2,Error)
+%Target_Set = [3;6;12];
+[w1,w2,Error] = BackPropagation(In_Set,Target_Set,Learning_Rate,w1,w2,Epocas)
 
 %% Uso de la red
 
-In = [1;2];
+In = pi;
 
-[Out] = Activation(In,w1,w2);
-disp(Out)
+[Out] = Activation(In,w1,w2)
